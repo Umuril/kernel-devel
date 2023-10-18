8639cb5ec1ca2221477586d19e62ad81e0f2de26
Eduard Rubio Cholbi <202501@studenti.unimore.it>
2023-10-18 13:14:33 +0200

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kernel Development
+
+### Download
+
+`git clone --recurse-submodules --shallow-submodules git@github.com:Umuril/kernel-devel.git`
--- scripts/regenerate-config.sh
+++ scripts/regenerate-config.sh
@@ -1,1 +1,3 @@
-../linux/scripts/kconfig/streamline_config.pl > ../build/.config
+#!/bin/bash
+
+../linux/scripts/kconfig/streamline_config.pl > ../build/.config 2> /dev/null
