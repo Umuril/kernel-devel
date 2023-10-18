0112ded1299312d18756c9107cc0a2f29785edd8
Eduard Rubio Cholbi <202501@studenti.unimore.it>
2023-10-18 23:12:03 +0200

    Fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 # https://docs.kernel.org/kbuild/reproducible-builds.html
 export KBUILD_BUILD_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S'`
 
-make -C ../linux -j$(nproc) O=../build olddefconfig && 
 make -C ../linux -j$(nproc) O=../build && 
 make -C ../linux -j$(nproc) O=../build modules
--- scripts/regenerate-config.sh
+++ scripts/regenerate-config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+make -C ../linux -j$(nproc) O=../build olddefconfig && 
 make -C ../linux -j$(nproc) O=../build localmodconfig
