bf3cc4e12b09c1f6c53edb347ec39fb63106e7e5
Eduard Rubio Cholbi <202501@studenti.unimore.it>
2023-11-08 16:36:18 +0100

    Add tools and ccache

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ### Download
 
 `git clone --recurse-submodules --shallow-submodules git@github.com:Umuril/kernel-devel.git`
+
+
+### Tools to install
+
+`qemu-full`
+
+`libvirt` (with the ssh and nat tools)
+
+`virt-manager`
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 # https://docs.kernel.org/kbuild/reproducible-builds.html
 export KBUILD_BUILD_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S'`
+export CC="ccache gcc"
 
-make -C ../linux -j$(nproc) O=../build && 
-make -C ../linux -j$(nproc) O=../build modules
+make CC="ccache gcc" -C ../linux -j$(nproc) O=../build && 
+make CC="ccache gcc" -C ../linux -j$(nproc) O=../build modules
